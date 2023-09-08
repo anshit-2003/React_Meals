192e036d0ab437336e8c0d6946b520fbce628bcb
Anshit Bakshi <84705984+anshit-2003@users.noreply.github.com>
2023-09-09 05:09:29 +0530

    Adding Firebase Support

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -1,36 +1,35 @@
+import { useEffect } from "react";
 import Card from "../UI/Card";
+import axios from "axios";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
-
-const DUMMY_MEALS = [
-    {
-        id: "m1",
-        name: "Sushi",
-        description: "Finest fish and veggies",
-        price: 22.99,
-    },
-    {
-        id: "m2",
-        name: "Schnitzel",
-        description: "A german specialty!",
-        price: 16.5,
-    },
-    {
-        id: "m3",
-        name: "Barbecue Burger",
-        description: "American, raw, meaty",
-        price: 12.99,
-    },
-    {
-        id: "m4",
-        name: "Green Bowl",
-        description: "Healthy...and green...",
-        price: 18.99,
-    },
-];
+import { useState } from "react";
 
 export default function AvailableMeals() {
-    const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} {...meal} />);
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        async function fetchMeals() {
+            try {
+                const response = await axios.get(
+                    "https://react-meals-7a8d7-default-rtdb.firebaseio.com/Meals.json"
+                );
+                const data = response.data;
+                for (const key in data) {
+                    const meal = {
+                        id: key,
+                        name: data[key].name,
+                        description: data[key].description,
+                        price: data[key].price,
+                    };
+                    setMeals((prevMeals) => [...prevMeals, meal]);
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        fetchMeals();
+    }, []);
+    const mealList = meals.map((meal) => <MealItem key={meal.id} {...meal} />);
     return (
         <section className={classes.meals}>
             <Card>
